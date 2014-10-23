19669eed482c1d4f5654a6e2aff8d47a9657a040
Nicola Gigante <nicola.gigante@gmail.com>
2014-10-23 18:44:37 +0200

    CSG user facing classes

--- include/csg.h
+++ include/csg.h
@@ -19,59 +19,66 @@
 
 #include <glm/glm.hpp>
 
-#include <memory>
+#include <std14/memory>
 
 namespace ocmesh {
 
     namespace details {
-        template<typename Derived>
-        class factory_t {
+        /*
+         * User facing primitives and operations.
+         */
+        class CSG {
         public:
-            template<typename ...Args>
-            static auto make() ->
-            {
-                return std::unique_ptr<Derived>(new Derived(std::forward<Args>(args)));
-            }
+            virtual ~CSG() = default;
         };
+        
+        using csg_ptr = std::unique_ptr<CSG>;
+        
+        class sphere_t : public CSG
+        {
+            float _radius;
+            
+        public:
+            sphere_t(float radius) : _radius(radius) { }
+            
+            float radius() const { return _radius; }
+        };
+        
+        class plane_t : public CSG
+        {
+            glm::vec4 _normal;
+            
+        public:
+            plane_t(glm::vec4 const&normal) : _normal(normal) { }
+            
+            glm::vec4 normal() const { return _normal; }
+        };
+        
+        class transform_t : public CSG
+        {
+            csg_ptr   _child;
+            glm::mat4 _matrix;
+            
+        public:
+            transform_t(csg_ptr child, glm::mat4 const&matrix)
+                : _child(std::move(child)) { }
+            
+            CSG *child() const { return _child.get(); }
+            glm::mat4 const&matrix() const { return _matrix; }
+        };
+        
+        inline csg_ptr sphere(float radius) {
+            return std14::make_unique<sphere_t>(radius);
+        }
+        
+        inline csg_ptr plane(glm::vec4 normal) {
+            return std14::make_unique<plane_t>(normal);
+        }
+        
+        inline csg_ptr transform(csg_ptr child, glm::mat4 const&matrix) {
+            return std14::make_unique<transform_t>(std::move(child), matrix);
+        }
     }
-    
-    /*
-     * User facing primitives and operations.
-     */
-    class CSG {
-    public:
-        virtual ~CSG() = default;
-    };
-    
-    using csg_ptr = std::unique_ptr<CSG>;
-    
-    class Sphere : public CSG
-    {
-        float _radius;
-        
-    public:
-        Sphere(float radius) : _radius(radius) { }
-        
-        float radius() const { return _radius; }
-    };
-    
-    class Plane : public CSG
-    {
-        glm::vec4 _normal;
-        
-    public:
-        Plane(glm::vec4 const&normal) : _normal(normal) { }
-        
-        glm::vec4 normal() const { return _normal; }
-    };
-
-    class Translation : public CSG {
-        csg_ptr _child;
-    public:
-        Translation(csg_ptr child) : _child(std::move(child)) { }
-        
-        CSG *child() const { return _child.get(); }
-    };
 }
 
 #endif
